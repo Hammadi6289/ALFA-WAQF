fa1c62ba189f8411eb3398d0e4f52a0afe31341f
Hammad Chohan <Hammad6289@gmail.com>
2026-01-13 19:46:49 +0500

    initialized the project.

---
+++ client/README.md
@@ -0,0 +1,7 @@
+# React + Vite Project for the Alfalah Hospital
+
+### Commits.
+
+1. Added Bootstrap CDN for styling.
+2. Added font-awesome CDN.
+3. Decide Basic routes?
---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="fa-solid fa-house"> Welcome</h1>
+    </>
+  );
+}
+
+export default App;
